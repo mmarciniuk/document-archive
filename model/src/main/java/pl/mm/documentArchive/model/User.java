ca76fe4ee23fd070eb387d111fee8b8379937c54
Mariusz Marciniuk <mariusz2048@gmial.com>
2019-08-21 23:27:45 +0200

    Correcting User model.

--- model/src/main/java/pl/mm/documentArchive/model/User.java
+++ model/src/main/java/pl/mm/documentArchive/model/User.java
@@ -22,21 +22,22 @@ public class User extends TransactionInfo {
 	@Column(name = "UserName", unique = true, nullable = false, length = 50)
 	private String userName;
 
-	@Length(min = 5, max = 50, message = "Password need contains at least 5 characters and at most 80 characters.")
+	@Length(min = 5, max = 80, message = "Password need contains at least 5 characters and at most 80 characters.")
 	@NotBlank(message = "Password cannot be empty.")
 	@Column(name = "Password", nullable = false, length = 80)
 	private String password;
 
-	@Length(min = 5, max = 50, message = "First name need contains at least 5 characters and at most 50 characters.")
+	@Length(min = 1, max = 50, message = "First name need contains at least 1 character and at most 50 characters.")
 	@NotBlank(message = "First name cannot be empty.")
 	@Column(name = "FirstName", nullable = false, length = 50)
 	private String firstName;
 
-	@Length(min = 5, max = 50, message = "Last name need contains at least 5 characters and at most 50 characters.")
+	@Length(min = 1, max = 50, message = "Last name need contains at least 1 character and at most 50 characters.")
 	@NotBlank(message = "Last name cannot be empty.")
 	@Column(name = "LastName", nullable = false, length = 50)
 	private String lastName;
 
+	@Length(max = 80, message = "Email address need contains at most 80 characters.")
 	@Pattern(regexp = "(^[A-z0-9]+)(@)([A-z]+)(\\.)([A-z]+$)", message = "Email address has incorrect format.")
 	@NotBlank(message = "Email address cannot be empty.")
 	@Column(name = "AddressEmail", nullable = false, length = 80)
