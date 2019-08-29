d82790519432caf9b49477dc5df56025f779c482
Mariusz Marciniuk <mariusz2048@gmial.com>
2019-08-30 01:13:44 +0200

    Optimizing TestDataProviders. Adding test data to module daoRepository from module testHelpers. Removing redundant test resources.

--- daoRepository/src/test/java/pl/mm/documentArchive/daoRepository/dataProvider/DocumentMetaDataTestDataProvider.java
+++ daoRepository/src/test/java/pl/mm/documentArchive/daoRepository/dataProvider/DocumentMetaDataTestDataProvider.java
@@ -1,53 +1,18 @@
 package pl.mm.documentArchive.daoRepository.dataProvider;
 
 
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.core.io.ClassPathResource;
 import org.testng.annotations.DataProvider;
-import pl.mm.documentArchive.model.Document;
-import pl.mm.documentArchive.model.DocumentMetaData;
-import pl.mm.documentArchive.model.User;
-import pl.mm.documentArchive.testHelpers.DataProviderHelper;
-
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import pl.mm.documentArchive.testHelpers.dataProviderLoader.DataProviderLoaderFromXlsDocumentMetaData;
 
 public class DocumentMetaDataTestDataProvider {
 
 	public static final String DOCUMENT_META_DATA_TEST_DATA_PROVIDER_NAME = "DOCUMENT_META_DATA_TEST_DATA_PROVIDER_NAME";
 
 	@DataProvider(name = DOCUMENT_META_DATA_TEST_DATA_PROVIDER_NAME)
-	public Object[][] testDocuments() throws IOException {
-		ClassPathResource classPathResource = new ClassPathResource("./documentMetaDataList/documentMetaDataList.xlsx");
-		XSSFWorkbook workbook = new XSSFWorkbook(classPathResource.getInputStream());
-		XSSFSheet sheet = workbook.getSheet("documentMetaDataList");
-
-		List<DocumentMetaData> documentMetaDataList = new ArrayList<>();
-		for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
-			XSSFRow row = sheet.getRow(i);
-
-			User ownerOfDocumentFromSheet = new User();
-			ownerOfDocumentFromSheet.setUserName(row.getCell(0).getStringCellValue());
-
-			Document documentNameFromSheet = new Document();
-			documentNameFromSheet.setDocumentName(row.getCell(1).getStringCellValue());
-			documentNameFromSheet.setDocumentOwner(ownerOfDocumentFromSheet);
-
-			DocumentMetaData documentMetaDataFromSheet = new DocumentMetaData();
-			documentMetaDataFromSheet.setUuid(UUID.randomUUID().toString());
-			documentMetaDataFromSheet.setCreatedDateTime(new Timestamp(System.currentTimeMillis()));
-			documentMetaDataFromSheet.setName(row.getCell(2).getStringCellValue());
-			documentMetaDataFromSheet.setValue(row.getCell(3).getStringCellValue());
-			documentMetaDataFromSheet.setDocument(documentNameFromSheet);
-
-			documentMetaDataList.add(documentMetaDataFromSheet);
-		}
-		return DataProviderHelper.convertToObject(documentMetaDataList);
+	public Object[][] testDocuments() throws Exception {
+		DataProviderLoaderFromXlsDocumentMetaData loaderFromXlsDocumentMetaData =
+				new DataProviderLoaderFromXlsDocumentMetaData("./testData/testData.xlsx", "documentMetaDataList");
+		return loaderFromXlsDocumentMetaData.loadTestData();
 	}
 
 }
--- daoRepository/src/test/java/pl/mm/documentArchive/daoRepository/dataProvider/DocumentTestDataProvider.java
+++ daoRepository/src/test/java/pl/mm/documentArchive/daoRepository/dataProvider/DocumentTestDataProvider.java
@@ -1,52 +1,16 @@
 package pl.mm.documentArchive.daoRepository.dataProvider;
 
-import org.apache.commons.compress.utils.IOUtils;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.core.io.ClassPathResource;
 import org.testng.annotations.DataProvider;
-import pl.mm.documentArchive.model.Document;
-import pl.mm.documentArchive.model.User;
-import pl.mm.documentArchive.testHelpers.DataProviderHelper;
-
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import pl.mm.documentArchive.testHelpers.dataProviderLoader.DataProviderLoaderFromXlsDocument;
 
 public class DocumentTestDataProvider {
 
 	public static final String DOCUMENTS_TEST_DATA_PROVIDER_NAME = "documentsTestDataProvider";
 
 	@DataProvider(name = DOCUMENTS_TEST_DATA_PROVIDER_NAME)
-	public Object[][] testDocuments() throws IOException {
-		ClassPathResource classPathResource = new ClassPathResource("/documentsList/documentsList.xlsx");
-		XSSFWorkbook workbook = new XSSFWorkbook(classPathResource.getInputStream());
-		XSSFSheet sheet = workbook.getSheet("documentsList");
-
-		List<Document> documents = new ArrayList<>();
-		for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
-			XSSFRow row = sheet.getRow(i);
-
-			User userFromSheet = new User();
-			userFromSheet.setUserName(row.getCell(0).getStringCellValue());
-
-			ClassPathResource classPathResourceDocument = new ClassPathResource(row.getCell(1).getStringCellValue());
-			Document documentFromSheet = new Document();
-			documentFromSheet.setUuid(UUID.randomUUID().toString());
-			documentFromSheet.setCreatedDateTime(new Timestamp(System.currentTimeMillis()));
-			documentFromSheet.setDocumentName(classPathResourceDocument.getFilename());
-			String[] split = Objects.requireNonNull(classPathResourceDocument.getFilename()).split("\\.");
-			documentFromSheet.setExtension(split[1]);
-			documentFromSheet.setDocumentBlob(IOUtils.toByteArray(classPathResource.getInputStream()));
-			documentFromSheet.setDocumentOwner(userFromSheet);
-
-			documents.add(documentFromSheet);
-		}
-		return DataProviderHelper.convertToObject(documents);
+	public Object[][] testDocuments() throws Exception {
+		DataProviderLoaderFromXlsDocument loaderFromXlsDocument = new DataProviderLoaderFromXlsDocument("/testData/testData.xlsx", "documentsList");
+		return loaderFromXlsDocument.loadTestData();
 	}
 
 }
